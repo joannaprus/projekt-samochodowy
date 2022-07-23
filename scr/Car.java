0c5bf82a52fd174a2fcdf4eb1b02cb34003d095e
joannaprus <joanna.m.prus@gmail.com>
2022-07-23 18:57:15 +0200

    wprowadzenie do obiektowości

--- scr/Car.java
+++ scr/Car.java
@@ -4,7 +4,7 @@ public class Car {
     private String brand;
     private boolean automaticGearbox;
 
-    public Car (String colout, int doorsAmount, String brand, boolean automaticGearbox){
+    public Car (String colour, int doorsAmount, String brand, boolean automaticGearbox){
         this.colour = colour;
         this.doorsAmount = doorsAmount;
         this.brand = brand;
@@ -14,8 +14,25 @@ public class Car {
     public void setAutomaticGearbox(boolean automaticGearbox){
         this.automaticGearbox = automaticGearbox;
     }
-public boolean getautomaticGearbox() {
+    public void setColour(String colour){
+        this.colour = colour;
+    }
+    public void setDoorsAmount(int doorsAmount){
+        this.doorsAmount = doorsAmount;
+    }
+    public void setBrand(String brand){
+        this.brand = brand;
+    }
+    public boolean getautomaticGearbox() {
         return automaticGearbox;
-
-}
+    }
+    public String getColour() {
+        return colour;
+    }
+    public int getDoorsAmount() {
+        return doorsAmount;
+    }
+    public String getBrand(){
+        return brand;
+    }
 }
--- scr/Main.java
+++ scr/Main.java
@@ -5,5 +5,11 @@ public class Main {
         System.out.println(audi.getautomaticGearbox());
         audi.setAutomaticGearbox(true);
         System.out.println(audi.getautomaticGearbox());
+        System.out.println(audi.getColour());
+        audi.setColour("red");
+        System.out.println(audi.getDoorsAmount());
+        audi.setDoorsAmount(5);
+        System.out.println(audi.getBrand());
+        audi.setBrand("A4");
     }
 }
